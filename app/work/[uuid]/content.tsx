70a69e2ca7fe860bfcb72456a653e9c25250ebb1
thiagosaraiva <thiagosaraivacsouza@gmail.com>
2024-07-22 16:57:16 -0300

    fix(app): fix render content

--- app/work/[uuid]/content.tsx
+++ app/work/[uuid]/content.tsx
@@ -1,26 +1,43 @@
 "use client";
 import { Heading, Paragraph } from "@/_components";
-import { useParams } from "next/navigation";
-import useStore from "../store";
-import { formatDate } from "@/_utils/date";
 import { Badge } from "@/_components/ui/badge";
+import { formatDate } from "@/_utils/date";
+import { useParams } from "next/navigation";
+import { ReactNode } from "react";
+import useStore from "../store";
+
+const renderContent = ({ index, type, value }: { [key: string]: string }) => {
+  const props = { key: index, className: "mb-8 sm:mb-10" };
+
+  const components: { [key: string]: ReactNode } = {
+    heading1: <Heading {...props}>{value}</Heading>,
+    paragraph: <Paragraph {...props}>{value}</Paragraph>,
+  };
+  return components[type];
+};
 
 export default function WorkContent() {
   const params = useParams();
   const { pageWork } = useStore();
 
   const collection = pageWork?.contentCollection?.items;
-  const content = collection?.find((item) => item.slug === params.uuid);
+  const data = collection?.find((item) => item.slug === params.uuid);
+  const content = data?.content.json.content.map((item) => {
+    return {
+      type: item?.nodeType,
+      value: item?.content[0]?.value,
+    };
+  });
 
   return (
     <div>
       <Heading type="h3" className="mb-4 sm:mb-3">
-        {content?.title}
+        {data?.title}
       </Heading>
-      <div className="flex justify-between">
-        <Paragraph>{`${formatDate(content?.createdAt)} • ${content?.type}`}</Paragraph>
+      <div className="flex justify-between mb-8 sm:mb-10">
+        <Paragraph>{`${formatDate(data?.createdAt)} • ${data?.type}`}</Paragraph>
         <div className="flex gap-3 ml-6">
-          {content?.stack.map((item, index) => (
+          {data?.stack.map((item, index) => (
             <Badge
               key={index}
               className="bg-background-secondary texst-font-low cursor-pointer"
@@ -29,6 +46,11 @@ export default function WorkContent() {
             </Badge>
           ))}
         </div>
+      </div>
+      <div>
+        {content?.map(({ type, value }, index) =>
+          renderContent({ index: `${index}`, type, value }),
+        )}
       </div>
     </div>
   );
