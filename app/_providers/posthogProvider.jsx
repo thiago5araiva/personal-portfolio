603a0a1f829999b0723b3f80e95f26115a77187d
thiagosaraiva <thiagosaraivacsouza@gmail.com>
2024-08-19 18:12:46 -0300

    fix(component): posthog

--- app/_providers/posthogProvider.jsx
+++ app/_providers/posthogProvider.jsx
@@ -1,22 +1,45 @@
-'use client'
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
-import { useEffect } from 'react'
+"use client";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
 
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-  })
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  });
 }
-export default function PosthogProvider({ children }) {
+
+const PostHogPageView = () => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthog = usePostHog();
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    // Track pageviews
+    if (pathname && posthog) {
+      let url = window.origin + pathname;
+      if (searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`;
+      }
+      posthog.capture("$pageview", {
+        $current_url: url,
+      });
+    }
+  }, [pathname, searchParams, posthog]);
+
+  return null;
+};
+
+export const PHProvider = ({ children }) => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
         api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-        person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-      })
+        person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      });
     }
-  }, [])
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
-}
+  }, []);
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+};
+
+export default PostHogPageView;
--- app/template.tsx
+++ app/template.tsx
@@ -1,21 +1,28 @@
-import { ReactNode } from 'react'
-import { Footer, Navigation } from '@/_components/'
+import { ReactNode } from "react";
+import { Footer, Navigation } from "@/_components/";
 
-import QueryProvider from '@/_providers/queryProvider'
-import Head from 'next/head'
+import QueryProvider from "@/_providers/queryProvider";
+import { PHProvider } from "@/_providers/posthogProvider";
+import dynamic from "next/dynamic";
+
+const PostHogPageView = dynamic(() => import("@/_providers/queryProvider"), {
+  ssr: false,
+});
 
 type Props = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 export default async function RootTemplate({ children }: Props) {
   return (
-    <QueryProvider>
-      <main className="container mx-auto px-6 max-w-screen-lg">
-        <Navigation />
-        {children}
-        <Footer />
-      </main>
-    </QueryProvider>
-  )
+    <PHProvider>
+      <QueryProvider>
+        <main className="container mx-auto px-6 max-w-screen-lg">
+          <Navigation />
+          {children}
+          <Footer />
+        </main>
+      </QueryProvider>
+    </PHProvider>
+  );
 }
