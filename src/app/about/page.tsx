90bf347c664ab7be08f57af4c263f291f37bfa3d
Thiago Saraiva <thiagosaraiva@Thiagos-MacBook-Pro.local>
2026-02-04 11:36:52 -0300

    refactor: simplify About page component
    Remove unused model dependency and pass empty state/action objects directly.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,5 @@
-'use client'
-import useAboutModel from './about.model'
 import AboutView from './about.view'
+
 export default function AboutPage() {
-    const model = useAboutModel()
-    return <AboutView {...model} />
+    return <AboutView state={{}} action={{}} />
 }
